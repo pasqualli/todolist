a4efa8f2a6a560b401fc43588e08a576bb1668ca
Antonio Maucyr Pasqualli Filho <am.pasqualli@gmail.com>
2023-10-02 11:14:36 -0300

    Update index.tsx

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -14,35 +14,42 @@ interface IProps {
   completeTodo: (id: string) => void;
 }
 
-export const List = ({ todos, deleteTodo, completeTodo }: IProps) => {
+export const List = ({ todos,completeTodo,deleteTodo }: IProps) => {
   return (
     <>
       {todos.map((todo, index) => {
-        
         return (
           <div className={styles.card} key={index}>
-            <input type="text" value={todo.description} />
+            {todo.completed ? (<input type="text" value={todo.description} />) : (<><input type="text" value={todo.description} />
             <div className={styles.icon}>
+              <span>
                 <FontAwesomeIcon
                   icon={faCheck}
                   color="#660096"
                   size="1x"
                   onClick={() => completeTodo(todo.id)}
                 />
+              </span>
+              <span>
                 <FontAwesomeIcon
                   icon={faPenToSquare}
                   color="#660096"
                   size="1x"
                 />
+              </span>
+              <span>
                 <FontAwesomeIcon
                   icon={faTrash}
                   color="#660096"
                   size="1x"
                   onClick={() => deleteTodo(todo.id)}
                 />
+              </span>
             </div>
+            </>)}
           </div>
         );
+        
       })}
     </>
   );
