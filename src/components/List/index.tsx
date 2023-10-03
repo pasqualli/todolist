fea573a8326801ecf8447e177b294b8dfac9324f
Antonio Maucyr Pasqualli Filho <am.paqualli@gmail.com>
2023-10-03 02:08:35 -0300

    conclusão do projeto

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -7,53 +7,102 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ITodo } from "../../pages/Home";
+import {
+  ChangeEvent,
+  ForwardRefRenderFunction,
+  createRef,
+  forwardRef,
+  useImperativeHandle,
+  useMemo,
+} from "react";
 
 interface IProps {
   todos: ITodo[];
   deleteTodo: (id: string) => void;
   completeTodo: (id: string) => void;
+  editTodo: (event: ChangeEvent<HTMLInputElement>, id: string) => void;
 }
 
-export const List = ({ todos,completeTodo,deleteTodo }: IProps) => {
+interface IListRef {
+  focus: (index: number) => void;
+}
+
+const ListBase: ForwardRefRenderFunction<IListRef, IProps> = (
+  { todos, completeTodo, deleteTodo, editTodo },
+  ref
+) => {
+  const inputRefs = useMemo(
+    () =>
+      Array(todos.length)
+        .fill(0)
+        .map(() => createRef<HTMLInputElement>()),
+    [todos.length]
+  );
+
+  //permite que o filho exporte funções para o pai
+  useImperativeHandle(ref, () => ({
+    focus: (index: number) => {
+      inputRefs[index].current?.focus();
+    },
+  }));
+  const handleTodoBlur = (index: number) => {
+    inputRefs[index].current?.focus();
+  };
   return (
     <>
       {todos.map((todo, index) => {
         return (
           <div className={styles.card} key={index}>
-            {todo.completed ? (<input type="text" value={todo.description} />) : (<><input type="text" value={todo.description} />
-            <div className={styles.icon}>
-              <span>
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  color="#660096"
-                  size="1x"
-                  onClick={() => completeTodo(todo.id)}
+            {todo.completed ? (
+              <input
+                type="text"
+                value={todo.description}
+                className={styles.inputCompleted}
+              />
+            ) : (
+              <>
+                <input
+                  type="text"
+                  value={todo.description}
+                  ref={inputRefs[index]}
+                  onChange={(e) => editTodo(e, todo.id)}
                 />
-              </span>
-              <span>
-                <FontAwesomeIcon
-                  icon={faPenToSquare}
-                  color="#660096"
-                  size="1x"
-                />
-              </span>
-              <span>
-                <FontAwesomeIcon
-                  icon={faTrash}
-                  color="#660096"
-                  size="1x"
-                  onClick={() => deleteTodo(todo.id)}
-                />
-              </span>
-            </div>
-            </>)}
+                <div className={styles.icon}>
+                  <span>
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      color="#2ff806"
+                      size="1x"
+                      onClick={() => completeTodo(todo.id)}
+                    />
+                  </span>
+                  <span>
+                    <FontAwesomeIcon
+                      icon={faPenToSquare}
+                      color="#f1e20c"
+                      size="1x"
+                      onClick={() => {
+                        handleTodoBlur(index);
+                      }}
+                    />
+                  </span>
+                  <span>
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      color="#f30707"
+                      size="1x"
+                      onClick={() => deleteTodo(todo.id)}
+                    />
+                  </span>
+                </div>
+              </>
+            )}
           </div>
         );
-        
       })}
     </>
   );
 };
-//<FontAwesomeIcon icon={faCheck} />
-//<FontAwesomeIcon icon={faTrash} />
-//<FontAwesomeIcon icon={faPenToSquare} />
+
+export const List = forwardRef(ListBase);
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,56 +1,98 @@
-import { useState } from 'react';
-import { Header } from '../../components/Header';
-import { List } from '../../components/List';
-import styles from './Home.module.css';
-import {v4 as uuid} from 'uuid';
+import { ChangeEvent, useEffect, useState } from "react";
+import { Header } from "../../components/Header";
+import { List } from "../../components/List";
+import styles from "./Home.module.css";
+import { v4 as uuid } from "uuid";
 
 export interface ITodo {
-    id: string;
-    description: string;
-    completed: boolean
+  id: string;
+  description: string;
+  completed: boolean;
 }
 
-export const Home = ()=> {
+export const Home = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
-    const [todo, setTodo] = useState<string>('');
-    const [todos, setTodos] = useState<ITodo[]>([]);
+  const [totalInProgress, setTotalInProgress] = useState(0);
+  const [totalCompleted, setTotalCompleted] = useState(0);
 
-    const addTodo = ()=> {
-        const newTodo = {
-            id: uuid(), description:todo, completed: false 
-        }
-        setTodos([...todos,newTodo ]);
-        setTodo('');
-    }
+  useEffect(() => {
+    const newTotalInProgress = todos.reduce(
+      (prev, current) => (!current.completed ? prev + 1 : prev),
+      0
+    );
+    setTotalInProgress(newTotalInProgress);
 
-    const deleteTodo = (id:string)=> {
-        const filterTodos = todos.filter(todo=> todo.id!== id)
-        setTodos(filterTodos);
-    }
-    const completeTodo = (id:string)=> {
-        const newTodosState = todos.filter((todo)=> {
-            if(todo.id ===id){
-                return {...todo, completed: !todo.completed}
-            }else{
-                return todo;
-            }
-        })
-        setTodos(newTodosState);
-    }
+    const newTotalCompleted = todos.reduce(
+      (prev, current) => (current.completed ? prev + 1 : prev),
+      0
+    );
+    setTotalCompleted(newTotalCompleted);
+  }, [todos]);
 
-    return (
-        <div >
-           <Header/>
-           <div className={styles.createTask}>
-            <input type="text" onChange={(e)=>setTodo(e.target.value)} value={todo} />
-            <button onClick={addTodo}>Adicionar</button>
-           </div>
-           <div className={styles.filter}>
-            <span className={styles.completed}>Finalizados: 5 tarefas</span>
-            <span className={styles.pending}>Em progresso: 5 tarefas</span>
-           </div>
+  const addTodo = () => {
+    const newTodo = {
+      id: uuid(),
+      description: todo,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setTodo("");
+  };
 
-           <List todos={todos} deleteTodo={deleteTodo} completeTodo={completeTodo}/>
-        </div>
-    )
-}
+  const deleteTodo = (id: string) => {
+    const filterTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filterTodos);
+  };
+  const completeTodo = (id: string) => {
+    const newTodosState = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newTodosState);
+  };
+
+  const editTodo = (event: ChangeEvent<HTMLInputElement>, id: string) => {
+    const newTodosState = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, description: event.target.value };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newTodosState);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.createTask}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button onClick={addTodo}>Adicionar</button>
+      </div>
+      <div className={styles.filter}>
+        <span className={styles.completed}>
+          Finalizados: {totalCompleted} tarefa(s)
+        </span>
+        <span className={styles.pending}>
+          Em progresso: {totalInProgress} tarefa(s)
+        </span>
+      </div>
+
+      <List
+        todos={todos}
+        deleteTodo={deleteTodo}
+        completeTodo={completeTodo}
+        editTodo={editTodo}
+      />
+    </div>
+  );
+};
